65f01954dfa2903b53d26c239e82f059ec8c0a4f
khaiphong <khaiphong.prajna@gmail.com>
2022-02-23 13:41:02 -0700

    Commerce

---
+++ src/pages/Relationships.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Relationships() {
+  return (
+    <div className='relationships'>
+      <p />
+      Commerce (Mu, CHR) relationships
+    </div>
+  );
+}
+
+export default Relationships;
